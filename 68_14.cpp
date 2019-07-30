62e8ddd978586e45d72f632b92a26079db36fb4b
kakawkaization <kakawkaization@gmail.com>
2019-07-30 22:27:18 +0300

    68 task 16

---
+++ 68_14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double jy;
+
+    cout << "Enter Jupiter years: ";
+    cin >> jy;
+
+    cout << jy << " Jupiter years equals " << jy * 12 << " Earth years.\n";
+
+    return 0;
+}
