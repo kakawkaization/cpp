3c362437ec6c0d7079c2e685078ad66821476eb8
kakawkaization <kakawkaization@gmail.com>
2019-08-15 06:04:53 +0300

    89

---
+++ 88.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double radius = 5.0, height = 5.0;
+
+    double volume = 3.1416 * radius * radius * height;
+
+    cout << "Volume of cylinder is equal " << volume << "\n";
+
+    return 0;
+}
