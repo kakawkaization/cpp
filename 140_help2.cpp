d28cf0cfb15c585e44bebc8a3e118de1b685724a
kakawkaization <kakawkaization@gmail.com>
2019-11-14 07:13:04 +0300

    143

--- 140_help2.cpp
+++ 140_help2.cpp
@@ -14,13 +14,41 @@ int main() {
         cout << "Choose variant of reference: ";
 
         cin >> choice;
-    } while(choice < '1' || cchoice > '5');
+    } while(choice < '1' || choice > '5');
 
-    cout << "\n\n"
+    cout << "\n\n";
 
     switch(choice) {
         case '1':
             cout << "Instruction if:\n\n";
-            cout << if(condition) instruction;\n";
+            cout << "if(condition) instruction;\n";
             cout << "else instruction;\n";
             break;
+        case '2':
+            cout << "Instruction switch:\n\n";
+            cout << "switch(expression) {\n";
+            cout << "    case constant:\n";
+            cout << "        sequence of instructions\n";
+            cout << "        break;\n";
+            cout << "    // ...\n";
+            cout << "}\n"; 
+            break;
+        case '3':
+            cout << "Instruction for:\n\n";
+            cout << "for(initialization; condition; increment)";
+            cout << " instruction;\n";
+            break;
+        case '4':
+            cout << "Instruction while:\n\n";
+            cout << "while(condition) instruction;\n";
+            break;
+        case '5':
+            cout << "Instruction do-while:\n\n";
+            cout << "do {\n";
+            cout << "    instruction;\n";
+            cout << "} while(condition);\n";
+            break;
+    }
+
+    return 0;
+}
